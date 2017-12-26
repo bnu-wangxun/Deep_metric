72add4d7f9648929514008e31ec06db78aa2706a
bnuwangxun <wx119141@qq.com>
2017-12-26 23:58:16 +0800

    bug 3-7

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=1 python train.py -data cub -r checkpoints/bn_cub_neig_1e5/800_model.pkl -start 800  -loss neighbour -net bn -BatchSiz 150 -num_instances 6 -epochs 1600 -lr 1e-5 -log_dir bn_cub_neig_1e5
+CUDA_VISIBLE_DEVICES=1 python train.py -data cub  -loss neighard -net bn -BatchSiz 150 -num_instances 6 -epochs 1600 -lr 1e-5 -log_dir bn_cub_neighard_1e5
 
