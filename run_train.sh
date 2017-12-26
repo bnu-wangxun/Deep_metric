cf3637d93162ea0b4bdf37d9e8396526aeb4e0ab
wangxun <xunwang@tuputech.com>
2017-12-26 11:35:21 +0800

    bug -8-3

--- run_train.sh
+++ run_train.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 python train.py -data cub -loss distance_match -BatchSize 128 -num_instances 8 -epochs 3000 -lr 1e-5 -cub_dist_1e-5
+CUDA_VISIBLE_DEVICES=0 python train.py -data cub -loss distance_match -BatchSize 128 -num_instances 8 -epochs 3000 -lr 1e-5 -log_dir cub_dist_1e-5
