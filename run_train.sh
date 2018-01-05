050b4f010dba5ea1e6d47401b67f7ae142cf44d2
wangxun <xunwang@tuputech.com>
2018-01-05 16:36:11 +0800

    new

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=8 python train.py -data car -net bn  -lr 1e-5 -num_instances 8 -BatchSize 128  -loss neighbour -epochs 1201 -log_dir car_m_01_1e5_n_8_b_128  -step 100 
+CUDA_VISIBLE_DEVICES=1 python train.py -data products -net bn  -lr 1e-5 -num_instances 4 -BatchSize 152  -loss neighbour -epochs 401 -log_dir products_m_01_1e5_n_8_b_152  -step 20 
 
