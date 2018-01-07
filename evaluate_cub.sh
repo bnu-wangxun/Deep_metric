a8d3f951e1ce37809c52d78e6cbc3546559877d9
wangxun <xunwang@tuputech.com>
2018-01-07 12:20:56 +0800

    bug 00

--- evaluate_cub.sh
+++ evaluate_cub.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=6 python test.py -data cub -r checkpoints/cub_m_01_1e5_n_8_b_128/400_model.pkl
+CUDA_VISIBLE_DEVICES=6 python test.py -data cub -r checkpoints/cub_adam_1e5_n_8_b_128//400_model.pkl
