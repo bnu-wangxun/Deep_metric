364084e7f71cb6b93cb6e4c1cf3d3e48200c7f3e
bnuwangxun <wx119141@qq.com>
2018-01-16 11:11:35 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=7 python train.py -data products -net bn  -lr 1e-5 -num_instances 4 -BatchSize 128  -loss neighbour -epochs 301 -log_dir products_m_01_1e5_n_4_b_128  -step 30 
+CUDA_VISIBLE_DEVICES=0 python train.py -data products -net bn  -lr 1e-5 -num_instances 4 -BatchSize 128  -loss neighbour -epochs 301 -log_dir products_m_01_1e5_n_4_b_128  -step 30 
 
