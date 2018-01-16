8b7ef6df7883bcb5e367e37575e46ef3375dff37
bnuwangxun <wx119141@qq.com>
2018-01-16 11:14:18 +0800

    hello
    master back

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 python train.py -data products -net bn  -lr 1e-5 -num_instances 4 -BatchSize 128  -loss neighbour -epochs 301 -log_dir products_m_01_1e5_n_4_b_128  -step 30 
+CUDA_VISIBLE_DEVICES=0 python train.py -data cub -net bn  -lr 1e-5 -num_instances 8 -BatchSize 128  -loss neighbour -epochs 401 -log_dir cub_m_01_1e5_n_8_b_128  -save_step 100 
 
